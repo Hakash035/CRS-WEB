02990abe997e620477c5bfae91424036cb2626f4
Hakash035 <42975048+Hakash035@users.noreply.github.com>
2024-06-16 10:10:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import {getFirestore} from "firebase/firestore/lite"
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsVSm4m8onf9PoVLWRIT6vKvnhQficdVQ",
-  authDomain: "idkh-e0303.firebaseapp.com",
-  projectId: "idkh-e0303",
-  storageBucket: "idkh-e0303.appspot.com",
-  messagingSenderId: "131403029187",
-  appId: "1:131403029187:web:beab440f459dc1c9686076"
+  // Firebase Config Here
 };
 
 const app = initializeApp(firebaseConfig);
